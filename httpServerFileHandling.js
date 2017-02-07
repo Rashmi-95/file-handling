ac76b29eb3dd8f5f637793f1431f40d42899add2
rashmi-95 <rashmi05rash@gmail.com>
2017-02-07 10:09:08 +0530

    reverted to api with get post put del methods

--- httpServerFileHandling.js
+++ httpServerFileHandling.js
@@ -1,35 +1,74 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fs = require('fs')
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json())
 const fileName = `/Users/rashmiranganathan/Documents/eslint/file-handling/readme.txt`
 
-const fileRead = require(`./fileRead.js`)
-const fileWrite = require(`./fileWrite.js`)
-const fileUpdate = require(`./fileUpdate.js`)
-const fileDelete = require(`./fileDelete.js`)
+app.get('/', function (req, res) {
+  res.send(`Welcome!!
+  * To read the file append '/read' to search bar
+  * To write to the file append '/write/text' to search bar
+  * To change a particular line in the file append '/update/line' to search bar & write line content into 'data'`)
+})
 
 app.get('/read', function (req, res) {
-  fileRead(fileName, res)
+  res.sendFile(fileName)
 })
 
-app.post('/write', function (req, res) {
+app.post('/write/:content', function (req, res) {
+  const contentToWrite = req.params.content
+  fs.appendFile(fileName, `\n${contentToWrite}`, function (err) {
+    if (err) throw err
+  })
+  res.send(`'${contentToWrite}' is written to the file`)
+})
+
+app.put('/update/:line', function (req, res) {
+  const lineNumber = req.params.line
   const data = req.body.data
-  fileWrite(fileName, data, res)
+  fs.readFile(fileName, function doneReading (error, fileContents) {
+    if (error) {
+      res.status(500).send('file not found')
+    } else {
+      const fileContentArray = fileContents.toString().split('\n')
+      if (fileContentArray.length < Number(lineNumber) + 1) {
+        res.status(500).send('line not found')
+      } else {
+        fileContentArray[lineNumber] = data
+        const fileContent = fileContentArray.join('\n')
+        fs.writeFile(fileName, fileContent, function (err) {
+          if (err) return console.log(err)
+        })
+        res.send(`The file is updated`)
+      }
+    }
+  })
 })
 
-app.post('/update', function (req, res) {
-  const lineNumber = req.body.line
-  const data = req.body.data
-  fileUpdate(fileName, lineNumber, data, res)
-})
+app.delete('/delete/:line', function (req, res) {
+  const lineNumber = req.params.line
+  fs.readFile(fileName, function doneReading (error, fileContents) {
+    if (error) {
+      res.status(500).send('file not found')
+    } else {
+      const fileContentArray = fileContents.toString().split('\n')
+      if (fileContentArray.length < Number(lineNumber) + 1) {
+        res.status(500).send('line not found')
+      } else {
+        fileContentArray.splice(lineNumber, 1)
+        const fileContent = fileContentArray.join('\n')
 
-app.post('/delete', function (req, res) {
-  const lineNumber = req.body.line
-  fileDelete(fileName, lineNumber, res)
+        fs.writeFile(fileName, fileContent, function (err) {
+          if (err) return console.log(err)
+        })
+        res.send(`The line number ${lineNumber} is deleted`)
+      }
+    }
+  })
 })
-
 app.listen(3006)
+
